531c5d6f38e520e87217c93996f8c5cc67f494aa
PaulPerez306 <93316260+Paw3lJ@users.noreply.github.com>
2022-06-07 18:23:28 +0200

    Dodawanie terminarzy dla doktorów
    oraz pobranie wizyty pacjenta na konsole

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 #Pobranie tokena uwierzytelniajacego
@@ -9,7 +10,8 @@ def pobierz_token():
 #pobierz_token()
 
 def menu():
-    print("Menu operacji na serwerze \n"
+    print("==========================\n"
+          "Menu operacji na serwerze \n"
           "==========================\n"
           "1. Lista endpointów na których można wykonać operację\n"
           "2. Wyswietl terminarz lekarza\n"
@@ -38,11 +40,50 @@ def pobierz_terminarz_lekarza():
     url_terminarz="http://127.0.0.1:8000/api/doctor/appointment/created/" + pk
     #token={'Authorization':f'Token {pobierz_token()}'}
     terminarz=requests.get(url_terminarz,
-                           params={'q': 'requests+language:python'},)
+                           params={'q': 'requests+language:python'})
     dane= terminarz.json()
     print(f'Identyfikator: {dane["id"]}')
-    print(f'Pełna nazwa lekarza: {dane["full_name"]}')
+    print(f'Imię i nazwisko doktora: {dane["full_name"]}')
+    print(f'Lokalizacja gabinetu: {dane["location"]}')
+    print(f'Godziny otwarcia gabinetu od: {dane["start_time"]} do {dane["end_time"]}')
+    print(f'Specjalizacja medyczna: {dane["qualification_name"]}')
+    print(f'Nazwa instytutu: {dane["institute_name"]}')
+    print(f'Nazwa szpitala: {dane["hospital_name"]}')
+    print(f'Wydział: {dane["department"]}\n')
 
+
+def utworz_terminarz():
+    full_name = input("Imię i nazwisko: ")
+    image=input("Zdjecie (domyślnie puste): ")
+    location=input("Miasto, Kraj: ")
+    start=input("Godzina otwarcia gabinetu: ")
+    end=input("Godzina zamknięcia gabinetu: ")
+    quali=input("Nazwa kwalifikacji: ")
+    institute=input("Instytut: ")
+    hospital=input("Nazwa szpitala: ")
+    department=input("Wydział szpitala: ")
+    created_at=input("Data stworzenia terminarza (domyślnie teraz): ")
+    user=input("ID użytkownika: ")
+
+    url_terminarz = "http://127.0.0.1:8000/api/doctor/appointment/"
+
+    data = {
+            'full_name': full_name,
+            'image': image,
+            'location':location,
+            'start_time':start,
+            'end_time':end,
+            'qualification_name':quali,
+            'institute_name':institute,
+            'hospital_name':hospital,
+            'department':department,
+            'created_at':created_at,
+            'user':user
+            }
+
+    res=requests.post(url_terminarz, data=data)
+    pastebin_url = res.text
+    print("The pastebin URL is:%s" % pastebin_url)
 
 def pobierz_liste_pacjentow():
     url_lista="http://127.0.0.1:8000/api/patient"
@@ -56,9 +97,17 @@ def pobierz_wizyty_pacjentow():
 
 def pobierz_wizyte_pacjenta():
     pk = input("Którą wizytę pokazać? ")
-    url_lista="http://127.0.0.1:8000/api/patient/appointment/created/"+pk
-    lista=requests.get(url_lista)
-    print(lista.text)
+    url_wizyta="http://127.0.0.1:8000/api/patient/appointment/created/"+pk
+    wizyta=requests.get(url_wizyta,
+                       params={'q': 'requests+language:python'})
+    dane = wizyta.json()
+    print(f'Identyfikator: {dane["id"]}')
+    print(f'Imię i nazwisko pacjenta: {dane["full_name"]}')
+    print(f'Opis dolegliwości: {dane["message"]}')
+    print(f'Numer telefonu pacjenta: {dane["phone_number"]}')
+    print(f'Data złożenia wizyty: {dane["date"]}')
+    print(f'Numer identyfikacyjny terminarza lekarskiego: {dane["appointment"]}\n')
+
 
 menu()
 active=True
@@ -70,6 +119,9 @@ while active:
         menu()
     elif ans=='2':
         pobierz_terminarz_lekarza()
+        menu()
+    elif ans=='3':
+        utworz_terminarz()
         menu()
     elif ans=='5':
         pobierz_liste_pacjentow()
