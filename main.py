202169d2da373b969679223205f30d695d8a07ee
PaulPerez306 <93316260+Paw3lJ@users.noreply.github.com>
2022-06-08 12:17:13 +0200

    Formatowanie wyświetlania danych, dodanie usuwania i tworzenia wizyt

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ def menu():
           "Menu operacji na serwerze \n"
           "==========================\n"
           "1. Lista endpointów na których można wykonać operację\n"
-          "2. Wyswietl terminarz lekarza\n"
-          "3. Dodaj terminarz lekarza\n"
-          "4. Usun terminarz lekarza\n"
-          "5. Wyswietl liste pacjentów\n"
-          "6. Wyswietl wizyty pacjentów\n"
-          "7. Wyswietl wizyte danego pacjenta\n"
-          "8. Dodaj wizyte pacjenta\n"
-          "9. Usun wizyte pacjenta\n"
-          "10. Wyswietl menu\n"
-          "11. Wyjscie\n"
+          "2. Wyswietl wszystkie terminarze\n"
+          "3. Wyswietl terminarz lekarza\n"
+          "4. Dodaj terminarz lekarza\n"
+          "5. Usun terminarz lekarza\n"
+          "6. Wyswietl liste pacjentów\n"
+          "7. Wyswietl wizyty pacjentów\n"
+          "8. Wyswietl wizyte danego pacjenta\n"
+          "9. Dodaj wizyte pacjenta\n"
+          "10. Usun wizyte pacjenta\n"
+          "11. Wyswietl menu\n"
+          "12. Wyjscie\n"
           "==========================\n")
 
 def endpointy():
@@ -33,6 +34,22 @@ def endpointy():
           "api/doctor/appointment/ - terminarz z wizytami lekarza\n"
           "api/doctor/appointment/created/pk - pojedynczy terminarz z wizytą lekarza, gdzie pk to id terminarza\n "
           "===================================================================")
+
+def wszystkie_terminarze():
+    url_terminarze = "http://127.0.0.1:8000/api/doctor/appointment"
+    wszystkie = requests.get(url_terminarze,
+                             params={'q': 'requests+language:python'})
+    dane= wszystkie.json()
+    for terminarz in dane:
+        print(f'Identyfikator: {terminarz["id"]}')
+        print(f'Imię i nazwisko doktora: {terminarz["full_name"]}')
+        print(f'Lokalizacja gabinetu: {terminarz["location"]}')
+        print(f'Godziny otwarcia gabinetu od: {terminarz["start_time"]} do {terminarz["end_time"]}')
+        print(f'Specjalizacja medyczna: {terminarz["qualification_name"]}')
+        print(f'Nazwa instytutu: {terminarz["institute_name"]}')
+        print(f'Nazwa szpitala: {terminarz["hospital_name"]}')
+        print(f'Wydział: {terminarz["department"]}\n')
+
 
 
 def pobierz_terminarz_lekarza():
@@ -81,19 +98,39 @@ def utworz_terminarz():
             'user':user
             }
 
-    res=requests.post(url_terminarz, data=data)
-    pastebin_url = res.text
-    print("The pastebin URL is:%s" % pastebin_url)
+    requests.post(url_terminarz, data=data)
+    print("Utworzono terminarz\n")
+
+def usun_terminarz():
+    pk=input("Podaj identyfikator terminarza który chcesz usunąć: ")
+    r = requests.delete('http://127.0.0.1:8000/api/doctor/appointment/created/'+ pk)
+
+    print(r.json())
 
 def pobierz_liste_pacjentow():
     url_lista="http://127.0.0.1:8000/api/patient"
-    lista=requests.get(url_lista)
-    print(lista.text)
+    lista=requests.get(url_lista,
+                       params={'q': 'requests+language:python'})
+
+    pacjenci = lista.json()
+    for pacjent in pacjenci:
+        print(f"Imię i nazwisko: {pacjent['full_name']}")
+        print(f"Numer telefonu pacjenta: {pacjent['phone_number']}")
+        print(f"Opis dolegliwości: {pacjent['message']}")
+        print(f"Data stworzenia wizyty: {pacjent['date']}\n")
 
 def pobierz_wizyty_pacjentow():
     url_lista="http://127.0.0.1:8000/api/patient/appointment"
-    lista=requests.get(url_lista)
-    print(lista.text)
+    wizyty=requests.get(url_lista,
+                       params={'q': 'requests+language:python'})
+    dane_wizyt = wizyty.json()
+    for wizyta in dane_wizyt:
+        print(f'Identyfikator: {wizyta["id"]}')
+        print(f'Imię i nazwisko pacjenta: {wizyta["full_name"]}')
+        print(f'Opis dolegliwości: {wizyta["message"]}')
+        print(f'Numer telefonu pacjenta: {wizyta["phone_number"]}')
+        print(f'Data złożenia wizyty: {wizyta["date"]}')
+        print(f'Numer identyfikacyjny terminarza lekarskiego: {wizyta["appointment"]}\n')
 
 def pobierz_wizyte_pacjenta():
     pk = input("Którą wizytę pokazać? ")
@@ -108,6 +145,35 @@ def pobierz_wizyte_pacjenta():
     print(f'Data złożenia wizyty: {dane["date"]}')
     print(f'Numer identyfikacyjny terminarza lekarskiego: {dane["appointment"]}\n')
 
+def utworz_wizyte():
+    full_name = input("Imię i nazwisko: ")
+    message = input("Opis dolegliwości: ")
+    phone= input("Numer telefonu pacjenta: ")
+    date = input("Data stworzenia wizyty: (domyślnie teraz): ")
+    user = input("ID pacjenta wstawiającego: ")
+    appointment = input("Numer terminarza do przywiązania: ")
+
+    url_wizyta = "http://127.0.0.1:8000/api/patient/appointment/"
+
+    data = {
+        'full_name': full_name,
+        'message': message,
+        'phone_number': phone,
+        'date': date,
+        'user': user,
+        'appointment': appointment
+    }
+
+    requests.post(url_wizyta, data=data)
+    print("Utworzono wizytę \n")
+
+
+def usun_wizyte():
+    pk = input("Podaj identyfikator wizyty, którą chcesz usunąć: ")
+    r = requests.delete('http://127.0.0.1:8000/api/patient/appointment/created/'+pk)
+
+    print(r.json())
+
 
 menu()
 active=True
@@ -118,21 +184,35 @@ while active:
         endpointy()
         menu()
     elif ans=='2':
-        pobierz_terminarz_lekarza()
+        wszystkie_terminarze()
         menu()
     elif ans=='3':
+        pobierz_terminarz_lekarza()
+        menu()
+    elif ans=='4':
         utworz_terminarz()
         menu()
     elif ans=='5':
+        usun_terminarz()
+        menu()
+    elif ans=='6':
         pobierz_liste_pacjentow()
         menu()
-    elif ans == '6':
+    elif ans == '7':
         pobierz_wizyty_pacjentow()
         menu()
-    elif ans=='7':
+    elif ans=='8':
         pobierz_wizyte_pacjenta()
         menu()
+    elif ans=='9':
+        utworz_wizyte()
+        menu()
+    elif ans=='10':
+        usun_wizyte()
+        menu()
     elif ans=='11':
+        menu()
+    elif ans=='12':
         active=False
         break
 
